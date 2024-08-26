0b2a3981ab0cadc44fc12ee4d3c0edb82893a7a2
Victor Botelho <victor_botelho_silva@hotmail.com>
2024-08-26 17:02:06 -0300

    Importação da Classe Scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,12 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /*To do: 
+        Conhecer e importar a classe Scanner; 
+        Exibir as mensagens para o usuário; 
+        Obter pela classe Scanner os valores digitados no terminal; 
+        Exibir a mensagem da conta criada.
+        */
+        Scanner input = new Scanner(System.in);
     }
 }
